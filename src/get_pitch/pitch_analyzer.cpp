4e9b3e62d411b7016bc4607c0247b802276b0a81
lirenete <lirenete@gmail.com>
2023-11-26 16:02:53 +0100

    problemes

--- src/get_pitch/pitch_analyzer.cpp
+++ src/get_pitch/pitch_analyzer.cpp
@@ -1,5 +1,125 @@
 /// @file
 
+// #include <iostream>
+// #include <math.h>
+// #include "pitch_analyzer.h"
+
+// using namespace std;
+
+// /// Name space of UPC
+// namespace upc {
+//   void PitchAnalyzer::autocorrelation(const vector<float> &x, vector<float> &r) const {
+
+//     for (unsigned int l = 0; l < r.size(); ++l) {
+//   		/// \TODO Compute the autocorrelation r[l]
+//       /** \FET 
+//        * - Inicialitzem ...
+//        * - Acumulem ...
+//        * - Dividim ...
+//        */ 
+//       for (unsigned int m=0; m< x.size()-l; ++m){
+//       r[l]+=x[m]*x[m+l];
+//       }
+//       r[l]=r[l]/x.size();
+//     }
+
+//     if (r[0] == 0.0F) //to avoid log() and divide zero 
+//       r[0] = 1e-10; 
+//   }
+
+//   void PitchAnalyzer::set_window(Window win_type) {
+//     if (frameLen == 0)
+//       return;
+
+//     window.resize(frameLen);
+
+//     switch (win_type) {
+//     case HAMMING:
+//       /// \TODO Implement the Hamming window
+//        for (unsigned int n = 0; n < frameLen; ++n)
+//           window[n] = 0.54 - 0.46 * cos(2 * 3.141592 * n / (frameLen - 1));
+
+//       break;
+//     case RECT:
+//     default:
+//       window.assign(frameLen, 1);
+//     }
+//   }
+
+//   void PitchAnalyzer::set_f0_range(float min_F0, float max_F0) {
+//     npitch_min = (unsigned int) samplingFreq/max_F0;
+//     if (npitch_min < 2)
+//       npitch_min = 2;  // samplingFreq/2
+
+//     npitch_max = 1 + (unsigned int) samplingFreq/min_F0;
+
+//     //frameLen should include at least 2*T0
+//     if (npitch_max > frameLen/2)
+//       npitch_max = frameLen/2;
+//   }
+
+//   bool PitchAnalyzer::unvoiced(float pot, float r1norm, float rmaxnorm) const {
+//     /// \TODO Implement a rule to decide whether the sound is voiced or not.
+//     /// * You can use the standard features (pot, r1norm, rmaxnorm),
+//     ///   or compute and use other ones. 
+//     //if(rmaxnorm > this->urmax && (r1norm > this->ur1 || pot>this->upot)){
+//      // return false; //sonor
+//     //}
+//     if(rmaxnorm >0.3){
+//       return false;
+//     }
+//     return true; //sord
+
+//   }
+
+//   float PitchAnalyzer::compute_pitch(vector<float> & x) const {
+//     if (x.size() != frameLen)
+//       return -1.0F;
+
+//     //Window input frame
+//     for (unsigned int i=0; i<x.size(); ++i)
+//       x[i] *= window[i];
+
+//     vector<float> r(npitch_max);
+
+//     //Compute correlation
+//     autocorrelation(x, r);
+
+//     vector<float>::const_iterator iR = r.begin(), iRMax = iR;
+
+//     /// \TODO 
+// 	/// Find the lag of the maximum value of the autocorrelation away from the origin.<br>
+// 	/// Choices to set the minimum value of the lag are:
+// 	///    - The first negative value of the autocorrelation.
+// 	///    - The lag corresponding to the maximum value of the pitch.
+//     ///	   .
+// 	/// In either case, the lag should not exceed that of the minimum value of the pitch.
+
+  
+//     for (iR=iRMax=(r.begin() + npitch_min); iR < (r.begin() + npitch_max); iR++){
+//       if (*iR > *iRMax)
+//         iRMax = iR;
+//     }
+      
+//     unsigned int lag = iRMax - r.begin();
+
+//     float pot = 10 * log10(r[0]);
+
+//     //You can print these (and other) features, look at them using wavesurfer
+//     //Based on that, implement a rule for unvoiced
+//     //change to #if 1 and compile
+// #if 0
+//     if (r[0] > 0.0F)
+//       cout << pot << '\t' << r[1]/r[0] << '\t' << r[lag]/r[0] << endl;
+// #endif
+    
+//     if (unvoiced(pot, r[1]/r[0], r[lag]/r[0]))
+//       return 0;
+//     else
+//       return (float) samplingFreq/(float) lag;
+//   }
+// }
+
 #include <iostream>
 #include <math.h>
 #include "pitch_analyzer.h"
@@ -12,15 +132,15 @@ namespace upc {
 
     for (unsigned int l = 0; l < r.size(); ++l) {
   		/// \TODO Compute the autocorrelation r[l]
-      /** \FET 
-       * - Inicialitzem ...
-       * - Acumulem ...
-       * - Dividim ...
-       */ 
-      for (unsigned int m=0; m< x.size()-l; ++m){
-      r[l]+=x[m]*x[m+l];
+      /// \TODO compute autocorrelacion
+
+      float acc = 0;
+      for (unsigned int n = l; n < x.size(); ++n){
+        acc += x[n] * x[n-l];
+        r[l] = acc/ x.size(); //Normalitzem.
+
       }
-      r[l]=r[l]/x.size();
+
     }
 
     if (r[0] == 0.0F) //to avoid log() and divide zero 
@@ -36,9 +156,8 @@ namespace upc {
     switch (win_type) {
     case HAMMING:
       /// \TODO Implement the Hamming window
-       for (unsigned int n = 0; n < frameLen; ++n)
-          window[n] = 0.54 - 0.46 * cos(2 * 3.141592 * n / (frameLen - 1));
-
+      for (unsigned int n = 0; n < frameLen; ++n)
+        window[n] = 0.54 - 0.46 * cos(2 * 3.141592 * n / (frameLen - 1));
       break;
     case RECT:
     default:
@@ -61,13 +180,11 @@ namespace upc {
   bool PitchAnalyzer::unvoiced(float pot, float r1norm, float rmaxnorm) const {
     /// \TODO Implement a rule to decide whether the sound is voiced or not.
     /// * You can use the standard features (pot, r1norm, rmaxnorm),
-    ///   or compute and use other ones. 
-    //if(rmaxnorm > this->urmax && (r1norm > this->ur1 || pot>this->upot)){
-     // return false; //sonor
-    //}
-    if(rmaxnorm >0.3)return false;
+    ///   or compute and use other ones.
+    if(rmaxnorm > 0.3 && (r1norm >  0.4 || pot> -16)){
+      return false; //sonor
+    }
     return true; //sord
-
   }
 
   float PitchAnalyzer::compute_pitch(vector<float> & x) const {
@@ -92,13 +209,11 @@ namespace upc {
 	///    - The lag corresponding to the maximum value of the pitch.
     ///	   .
 	/// In either case, the lag should not exceed that of the minimum value of the pitch.
-
-  
-    for (iR=iRMax=(r.begin() + npitch_min); iR < (r.begin() + npitch_max); iR++){
+    for (iR = iRMax = (r.begin() + npitch_min); iR < (r.begin() + npitch_max); iR++){
       if (*iR > *iRMax)
         iRMax = iR;
-    }
-      
+  }
+
     unsigned int lag = iRMax - r.begin();
 
     float pot = 10 * log10(r[0]);
